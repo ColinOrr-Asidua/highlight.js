d9bf1d28600c8ed1e859832fefa3d3e8a9fac14c
Colin Orr <colin.orr@asidua.com>
2012-09-18 08:24:10 +0100

    Added the Gherkin (EN) language ported over from https://github.com/cucumber/gherkin-syntax-highlighters.

---
+++ src/languages/gherkin_en.js
@@ -0,0 +1,32 @@
+/*
+Language: Gherkin (EN)
+*/
+
+function(hljs) {
+  return {
+    contains: [
+        {
+            className: 'keyword',
+            begin: '^\\s*(But |And |Then |When |Given |\\* |Scenarios|Examples|Scenario Template|Scenario Outline|Scenario|Background|Feature)',
+            relevance: 5
+        },
+        {
+            className: 'string',
+            begin: '\\|',
+            relevance: 0
+        },
+        hljs.HASH_COMMENT_MODE,
+        {
+            className: 'string',
+            begin: '"""', end: '"""',
+            relevance: 10
+        },
+        hljs.APOS_STRING_MODE, 
+        hljs.QUOTE_STRING_MODE,
+        hljs.C_NUMBER_MODE,
+        {
+            className: 'annotation', begin: '\\s+@[^@\r\n\t ]+'
+        }
+    ]
+  };
+}
